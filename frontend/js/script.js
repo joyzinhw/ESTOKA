c98f5f614556f5c7ae88ee763e5a6ffa1c8ae88f
Joyce Moura <joyzinhw@gmail.com>
2025-06-06 12:07:05 -0300

    firebase

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,5 +1,19 @@
 const apiURL = 'http://localhost:5000/produtos';
 
+const signUpButton=document.getElementById('signUpButton');
+const signInButton=document.getElementById('signInButton');
+const signInForm=document.getElementById('signIn');
+const signUpForm=document.getElementById('signup');
+
+signUpButton.addEventListener('click',function(){
+    signInForm.style.display="none";
+    signUpForm.style.display="block";
+})
+signInButton.addEventListener('click', function(){
+    signInForm.style.display="block";
+    signUpForm.style.display="none";
+})
+
 carregarProdutos();
 
 async function carregarProdutos() {
